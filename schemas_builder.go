0a5fba7f495eadbdfff6fa2370eee0bed59e3e6f
Ernest Micklei <ernest.micklei@gmail.com>
2016-03-09 21:52:39 +0100

    fix builder

--- schemas_builder.go
+++ schemas_builder.go
@@ -1,9 +1,11 @@
 package swagger
 
+import "github.com/emicklei/swagger2/model"
+
 type SchemasBuilder struct {
-	schemas map[string]Schema
+	schemas map[string]model.Schema
 }
 
 func NewSchemasBuilder() SchemasBuilder {
-	return SchemasBuilder{schemas: map[string]Schema{}}
+	return SchemasBuilder{schemas: map[string]model.Schema{}}
 }
